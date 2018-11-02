3e8dcf80e9edd5156ca416561189f200fd1b8d23
alexandru-dinu <alex.dinu07@gmail.com>
2018-11-02 14:03:29 +0200

    Try hsv-based contour search.

--- src/misc.py
+++ src/misc.py
@@ -1,12 +1,51 @@
 import argparse
 from termcolor import colored
 
-from filtering import strided_conv
+from filtering import strided_conv, filter_image, median_filtering
 from kernels import gaussian_kernel, box_kernel
 from utils import *
 
 
-def main(args):
+def hsv_red_contours(args):
+    orig_img = open_img(args.img_path)
+
+    img = cv2.imread(args.img_path)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    mask1 = cv2.inRange(
+        hsv, np.array([0, 110, 100]), np.array([10, 255, 255])
+    )
+    mask2 = cv2.inRange(
+        hsv, np.array([170, 110, 100]), np.array([180, 255, 255])
+    )
+
+    mask = (mask1 | mask2)# / 255
+    assert mask[mask == 255].size + mask[mask == 0].size == mask.size
+
+    out = median_filtering(np.expand_dims(mask, axis=2), args.size)
+    out = out[:, :, 0].astype(np.uint8)
+    _, contours, _ = cv2.findContours(out, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+    # draw all countours
+    # for i, cont in enumerate(contours):
+    #     bbox = cv2.boundingRect(cont)
+    #     tl, br = (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3])
+    #     cv2.rectangle(orig_img, tl, br, color=(0, 255, 0), thickness=3)
+
+    # draw only the largest contour
+    bboxes = [cv2.boundingRect(cont) for cont in contours]
+    areas = [b[2] * b[3] for b in bboxes]
+    bbox = bboxes[np.argmax(areas)]
+    tl, br = (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3])
+    cv2.rectangle(orig_img, tl, br, color=(0, 255, 0), thickness=3)
+
+    show_img(orig_img)
+
+    # TODO: return a list of possible bboxes and perform IOU with matchTemplate results
+
+
+def color_clusters(args):
     # assert args.filter in ['gaussian', 'box']
 
     img = open_img(args.img_path, args.img_mode)
@@ -24,10 +63,11 @@ def main(args):
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--img_path', type=str)
-	parser.add_argument('--img_mode', type=str)
-	# parser.add_argument('--filter', type=str)
-	parser.add_argument('--size', type=int)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img_path', type=str)
+    parser.add_argument('--img_mode', type=str)
+    # parser.add_argument('--filter', type=str)
+    parser.add_argument('--size', type=int)
 
-	main(parser.parse_args())
+    # color_clusters(parser.parse_args())
+    hsv_red_contours(parser.parse_args())
