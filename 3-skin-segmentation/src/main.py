906e3e7f03d1ff526137bf2af948de11642c7463
alexandru-dinu <alex.dinu07@gmail.com>
2018-11-16 16:17:33 +0200

    Updates.

--- 3-skin-segmentation/src/main.py
+++ 3-skin-segmentation/src/main.py
@@ -2,40 +2,97 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import argparse
-import imutils
+
+np.set_printoptions(threshold=np.nan, linewidth=240)
 
 
-def _test():
-	lower = np.array([0, 48, 80], dtype="uint8")
-	upper = np.array([20, 255, 255], dtype="uint8")
-	camera = cv2.VideoCapture(0)
-	# keep looping over the frames in the video
-	while True:
-		# grab the current frame
-		(grabbed, frame) = camera.read()
+def show(arr):
+	plt.imshow(arr, cmap='gray')
+	plt.tight_layout()
+	plt.show()
 
-		# if we are viewing a video and we did not grab a
-		# frame, then we have reached the end of the video
-		if not grabbed:
-			break
 
-		# resize the frame, convert it to the HSV color space,
-		# and determine the HSV pixel intensities that fall into
-		# the speicifed upper and lower boundaries
-		frame = imutils.resize(frame, width=400)
-		converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-		skinMask = cv2.inRange(converted, lower, upper)
-		# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-		# skinMask = cv2.dilate(skinMask, kernel, iterations=1)
-		# skinMask = cv2.erode(skinMask, kernel, iterations=1)
-		# skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+def hsv_thresholding(hsv, low_thr, high_thr):
+	"""
+	HSV -> Binary image
+	"""
+	mask = cv2.inRange(
+		hsv, np.array(args.low_thr), np.array(args.high_thr)
+	)
 
-		# show the skin in the image along with the mask
-		cv2.imshow("images", np.hstack([cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), skinMask]))
+	assert mask[mask == 255].size + mask[mask == 0].size == mask.size
 
-		# if the 'q' key is pressed, stop the loop
-		if cv2.waitKey(1) & 0xFF == ord("q"):
-			break
+	if False:
+		strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+		# mask = cv2.medianBlur(mask, ksize=7)
+		mask = cv2.erode(mask, strel, iterations=2)
+		mask = cv2.dilate(mask, strel, iterations=3)
+
+	return mask
+
+
+def noisy_cc_remove(img, num_iter=5):
+	"""
+	Binary image -> Binary image
+	"""
+	strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+
+	out = img.copy()
+	out = cv2.erode(out, strel, iterations=1)
+	out = cv2.dilate(out, strel, iterations=2)
+
+	for _ in range(num_iter):
+		out = cv2.dilate(out, strel, iterations=3)
+		out = cv2.erode(out, strel, iterations=3)
+
+	return out
+
+
+def blob_detection(img):
+	"""
+	Binary image -> [blob]
+	"""
+	h, w = img.shape
+
+	lbl = 2
+	blobs = []
+
+	# for y in range(h):
+	# 	for x in range(w):
+	# 		if img[y, x] != 1:
+	# 			continue
+
+	# 		num, im, msk, rect = cv2.floodFill(img, None, (y+20, x), lbl)
+	# 		ry, rx, rh, rw = rect
+
+	# 		blob = []
+
+	# 		for i in range(ry, ry + rh):
+	# 			for j in range(rx, rx + rw):
+	# 				if (img[i, j] != lbl):
+	# 					continue
+	# 				blob.append((i, j))
+
+	# 		blobs.append(blob)
+	# 		lbl += 1
+
+	# return blobs
+
+
+def ellipse_fitting(img, blobs):
+	"""
+	for each blob
+		l1, l2 = eigenvalues(blob)
+		if 1 <= l1/l2 <= 2
+			fit & draw ellipse
+	"""
+
+	for x in blobs:
+		xp = np.matrix(x - x.mean(axis=0))
+		s = (1 / (len(xp) - 1)) * xp.T * xp
+		evalues, evectors = np.linalg.eig(s)
+		m_evalues = evalues + x.mean(axis=0)
+		return m_evalues
 
 
 def process_single(args):
@@ -43,28 +100,13 @@ def process_single(args):
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-	mask = cv2.inRange(
-		hsv, np.array(args.low_thr), np.array(args.high_thr)
-	)
+	# --low_thr 0 50 150 --high_thr 25 255 255
+	mask = hsv_thresholding(hsv, args.low_thr, args.high_thr)
+	# mask = np.divide(mask, 255).astype(np.uint8)
 
-	assert mask[mask == 255].size + mask[mask == 0].size == mask.size
+	out = noisy_cc_remove(mask)
 
-	strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-
-	# mask = cv2.medianBlur(mask, ksize=7)
-	mask = cv2.erode(mask, strel, iterations=2)
-	# mask = cv2.medianBlur(mask, ksize=11)
-	# mask = cv2.erode(mask, strel, iterations=3)
-	mask = cv2.dilate(mask, strel, iterations=3)
-	# mask = cv2.erode(mask, strel, iterations=3)
-	# mask = cv2.dilate(mask, strel, iterations=20)
-	# mask = cv2.erode(mask, strel, iterations=5)
-	# mask = cv2.medianBlur(mask, ksize=11)
-
-	plt.imshow(np.vstack((gray, mask)), cmap='gray')
-	# plt.imshow(mask, cmap='gray')
-	plt.tight_layout()
-	plt.show()
+	show(np.vstack((out, mask)))
 
 
 def main(args):
@@ -79,9 +121,4 @@ if __name__ == '__main__':
 	parser.add_argument('--debug', action='store_true')
 	args = parser.parse_args()
 
-	print(args)
-
-	if not args.debug:
-		main(args)
-	else:
-		_test()
+	main(args)
--- 3-skin-segmentation/src/test.py
+++ 3-skin-segmentation/src/test.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import imutils
+import cv2
 
-# Generate fake data.
+np.set_printoptions(suppress=True, precision=4)
 
 rs = np.random.RandomState(12345)
 
@@ -16,16 +18,35 @@ X = np.c_[a * np.cos(2 * np.pi * t), b * np.sin(2 * np.pi * t)]
 X = X + rs.randn(N, 2) * 0.7
 X = np.dot(rotation_matrix(np.pi/6), X.T).T
 
-
 # Fit the ellipse.
 u, s, vt = np.linalg.svd((X - X.mean(axis=0)) / np.sqrt(N), full_matrices=False)
+print(s)
+
+x = X
+xp = np.matrix(x - x.mean(axis=0))
+cm = (1 / (len(xp) - 1)) * xp.T * xp
+l, v = np.linalg.eig(cm)
+mv = v + x.mean(axis=0)
+
+v1 = (v[0,0], v[0,1])
+v2 = (v[1,0], v[1,1])
+
+ax = plt.axes()
+ax.arrow(0, 0, *v1, head_width=0.1, head_length=0.1)
+ax.arrow(0, 0, *v2, head_width=0.1, head_length=0.1)
+
+print(vt)
+print(v)
 
 ellipse = np.sqrt(2) * np.c_[s[0] * np.cos(2 * np.pi * t), s[1] * np.sin(2 * np.pi * t)]
-angle = np.arctan2(vt[0, 1], vt[0, 0])
+angle = np.arctan2(vt[0, 0], vt[1,0])
 ellipse = np.dot(rotation_matrix(angle), ellipse.T).T
+print(ellipse.shape)
 
-plt.plot(X[:, 0], X[:, 1], 'ob')
+plt.plot(x[:, 0], x[:, 1], 'ob')
 plt.plot(ellipse[:, 0], ellipse[:, 1], '-r', lw=3)
-plt.axis('equal')
+plt.ylim(-10,10)
+plt.xlim(-10,10)
+plt.tight_layout()
 plt.grid()
 plt.show()
